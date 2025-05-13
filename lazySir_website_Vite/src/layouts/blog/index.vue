0bf66bd1a38d849d384c76ccd5274f05688cd31b
lazySir <101635531+lazySir@users.noreply.github.com>
2025-05-13 14:21:05 +0800

    refactor: 重构（既不新增功能也不是修 bug）♻️
    ♻️ refactor: 1.删除blog的index页面无用代码
    ♻️ refactor: 2.删除之前blog的测试图片

--- lazySir_website_Vite/src/layouts/blog/index.vue
+++ lazySir_website_Vite/src/layouts/blog/index.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import navBar from '@/views/global/navbar/index.vue'
-// import NavBarMobile from '@/layouts/front/components/navbar_mobile.vue'
-// import Footer from '@/layouts/front/components/footer.vue'
 </script>
 
 <template>
@@ -9,27 +7,13 @@ import navBar from '@/views/global/navbar/index.vue'
     class="before:content-[''] before:fixed before:inset-0 before:-z-10 before:bg-[url('@/assets/images/blog/blog_bg.png')] before:opacity-[10%] before:bg-repeat before:min-h-full"
   >
     <header>
-      <!-- <navBar class="hidden tablet:block" /> -->
       <navBar />
-      <!-- <NavBarMobile class="block tablet:hidden" /> -->
     </header>
 
     <main>
       <router-view></router-view>
     </main>
-    <!-- <footer class="w-[100vw] h-[150px] tablet:w-full"><Footer /></footer> -->
   </div>
 </template>
 
-<style scoped>
-.test::bofore {
-  content: '';
-  inset: 0;
-  position: fixed;
-  top: 0;
-  z-index: -1;
-  background-image: url(../../assets/images/blog/blog_bg.png);
-  background-repeat: repeat;
-  min-height: 100%;
-}
-</style>
+<style scoped></style>
